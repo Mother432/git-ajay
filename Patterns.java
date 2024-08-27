94255b1280741fa6f6707527a029ca8624970926
Mother432 <kolasandhyarani312@gmail.com>
2024-08-27 19:14:30 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -21,6 +21,22 @@ public class Patterns
 			
 			System.out.println();
 			}
+			/*for(int j=i;j<=n;j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=1;j<i;j++)
+			{
+				
+				System.out.print("/ ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("/ ");
+			}
+			
+			System.out.println();
+			}*/
 			for(int i=1;i<=n;i++)
 			{
 				for(int j=1;j<=i;j++)
