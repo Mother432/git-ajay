e061c1402cc545b9066839dfdc759f43e5fdfd37
Mother432 <kolasandhyarani312@gmail.com>
2024-09-18 09:15:45 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -5,7 +5,6 @@ public class TriPattern {
             for (int j = 0; j < size - i - 1; j++) {
                 System.out.print(" ");
             }
-            // Print the pattern
             System.out.print(".");
             System.out.println();
         }
@@ -17,6 +16,79 @@ public class TriPattern {
             System.out.print(" ");
         }
         System.out.println(" \\ / ");
+    }
+}
+
+
+
+
+public class TriPattern1 {
+    public static void main(String[] args) {
+        int size = 3;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" /");
+                System.out.print(" ");
+            }
+            System.out.print(" ");
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" .");
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" \\");
+                System.out.print(" ");
+            }
+            
+            System.out.println();
+        }
+        for (int i = size - 1; i > 0; i--) {
+            for (int j = 0; j < size - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("/ ");
+                System.out.print(" ");
+            }
+            System.out.print(" ");
+            for (int j = 0; j < i; j++) {
+                System.out.print(". ");
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("\\ ");
+                System.out.print(" ");
+            }
+            
+            System.out.println();
+        }
+        for (int i = 1; i < size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+
+            // Print slashes and dots in the middle
+            for (int j = 0; j < size - i; j++) {
+                System.out.print(" /");
+                System.out.print(" ");
+            }
+            
+            // Print dots
+            System.out.print(" ");
+            for (int j = 0; j < size - i; j++) {
+                System.out.print(" .");
+                System.out.print(" ");
+	    }
+            for (int j = 0; j < size - i; j++) {
+                System.out.print(" \\");
+                System.out.print(" ");
+            }
+            
+            System.out.println();
+        }
     }
 }
 	
